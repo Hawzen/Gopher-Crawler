28701a687b2531caadd36ec56989406582e33914
Mohannad Alrasheed <43524721+Hawzen@users.noreply.github.com>
2023-08-25 03:03:50 +0300

    machine learning

--- analyzer/server.py
+++ analyzer/server.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
 from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import TfidfVectorizer
+from transformers import pipeline
 app = Flask(__name__)
 
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # Summarization
 @app.route('/summarize', methods=['POST'])
@@ -10,25 +13,24 @@ def summarize():
     if html == '':
         return 'No html provided'
     text = get_text_from_html(html)
+
     return summarize_text(text)
 
 def summarize_text(text: str) -> str:
-    # stub
-    return text
+    return summarizer(text, max_length=130, min_length=30, do_sample=False)
 
 
 # Keywords
 @app.route('/keywords', methods=['POST'])
 def keywords():
-    """stub"""
     html = request.data.decode('utf-8')
     text = get_text_from_html(html)
     return get_keywords(text)
 
-def get_keywords(text: str) -> list:
-    """stub"""
-    return ["apple", "banana", "orange"]
-
+def get_keywords(text: str, num_keywords: int = 10) -> list:
+    vectorizer = TfidfVectorizer(max_features=num_keywords)
+    vectorizer.fit_transform([text])
+    return vectorizer.get_feature_names_out().tolist()
 
 # Utils
 def get_text_from_html(html: str):
@@ -37,4 +39,4 @@ def get_text_from_html(html: str):
 
 
 if __name__ == '__main__':
-    app.run(port=6969)
+    app.run(port=9898)
